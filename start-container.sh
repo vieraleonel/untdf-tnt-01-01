b7075f4c9355ed92b6fd655415c45fcfb4df5d33
Leonel Viera <vieraleonel1@gmail.com>
2022-04-01 19:46:54 -0300

    New Start container script

--- start-container.sh
+++ start-container.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# chequeo si tengo node_modules y en caso de no tenerlo, instalo las dependencias
 [ -d node_modules ] || npm install
 
+# corremos el servidor
 nodemon index.js 3000
